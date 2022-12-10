de263e4e13bfaf3b9d4de69d2fc5026395fb78ab
ctmcarthur <ctmcarthur@gmail.com>
2022-12-10 14:46:00 -0800

    dayten solved

--- AdventLib/AdventDays/DayTen.h
+++ AdventLib/AdventDays/DayTen.h
@@ -9,10 +9,11 @@ namespace DayTen
         void PushNoop();
         void PushAdd(int32_t amt);
         int32_t GetValue(uint32_t cycleIdx) const;
+        const std::vector<int32_t>& GetCycles() const { return mCycles; }
 
     private:
         void FillToCycle(uint32_t newCycle);
-        uint32_t mCurrentCycle = 1;
+        uint32_t mCurrentCycle = 0;
         std::vector<int32_t> mCycles;
     };
 }
